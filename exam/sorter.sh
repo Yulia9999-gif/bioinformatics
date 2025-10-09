066c8519c48c8650ac336a32be2391cbaacaa013
Yulia9999-gif <likundra.4445@gmail.com>
2025-10-09 19:22:48 +0300

    Update sorter.sh

--- exam/sorter.sh
+++ exam/sorter.sh
@@ -1,4 +1,5 @@
-for filename in ./results/data*
-do
-    echo $(basename "$filename")
+for filename in ./results/data*; do
+    basename=$(basename "$filename" .txt)
+    sort "$filename" > "./results/${basename}_sorted.txt"
+    echo "Обработан файл: ${basename}_sorted.txt"
 done
